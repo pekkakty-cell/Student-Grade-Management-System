27c610a8df266df1c83a297fcdab2608afe0a94e
Taeyeong <tykim@ictpeople.co.kr>
2026-08-10 10:27:46 +0900

    chore: Gradle 프로젝트 초기 설정

---
+++ src/main/java/com/example/studentgraade/StudentGradeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentgraade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentGradeManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentGradeManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studentgraade/StudentGradeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentgraade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentGradeManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
